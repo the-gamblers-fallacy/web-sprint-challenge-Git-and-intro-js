697fcaa1bebbc9784274e201be849a8eb5c52a5c
Lucas Bigelow <lukejbigelow@gmail.com>
2022-01-14 16:32:58 -0600

    completed tasks 8 and 9 with passing tests, all tests pass

--- index.js
+++ index.js
@@ -338,8 +338,19 @@ Use artistInfo to do the following:
 For example artistInfo(artists, 'Frida Kahlo') will return: 
   ["Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."]
 */
-function artistInfo(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistInfo(array, artistName){
+  // tests are expecting a bio in an array, not just the bio string
+  let artistBio = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].name === artistName) {
+      artistBio.push(array[i].bio);
+
+      return artistBio;
+    }
+  }
+
+  return null;
 }
 
 
@@ -353,8 +364,16 @@ Use artistByCountry to do the following:
 For example artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya', 'El Greco' ]
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistByCountry(array, artistNationality){
+  const artistsByCountry = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].nationality === artistNationality) {
+      artistsByCountry.push(array[i].name);
+    }
+  }
+
+  return artistsByCountry;
 }
 
 
